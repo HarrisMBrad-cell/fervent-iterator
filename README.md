b3440ee8e528334cf7f3e1a1a0c623ce88e88435
HarrisMBrad <brforeal1@gmail.com>
2021-11-30 15:21:23 -0500

    added db files for mok iterations, deno for new way to use

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Created with CodeSandbox
 # New Feature
 
 With TS See features/annotations
+...
+
+## Note
+
+This is still WIP...
--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
+      <App>
+        Tap
+      </App>
       <h2>Start editing to see some magic happenen!</h2>
     </div>
+
+
+
   );
 }
